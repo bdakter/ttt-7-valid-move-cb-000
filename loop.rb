6c1ada4b024125073bce93b93b85eec2ab5a344f
Boruch Z <philomath613@gmail.com>
2019-02-27 21:22:40 +0000

    Automatically backed up by Learn

--- loop.rb
+++ loop.rb
@@ -1,13 +1,12 @@
 
 
 counter = 0 # Start our counter at 0, we have never run the loop
-loop {
-  counter = counter + 1
-
-  # Do Something
+loop do
+  counter += 1
+  
   puts "Iteration #{counter} of the loop"
 
-  if counter >= 10 # If our counter is 10 or more
-    break # Stop the loop
+  if counter >= 3
+    break 
   end
-}
+end
